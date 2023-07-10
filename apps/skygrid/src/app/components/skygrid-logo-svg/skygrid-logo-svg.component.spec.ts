e4b45550318be5a89548d99f76ce22163b53823d
Serkan Sipahi <serkan.sipahi@yahoo.de>
2023-07-10 08:20:39 +0200

    test(skygrid): fix

--- apps/skygrid/src/app/components/skygrid-logo-svg/skygrid-logo-svg.component.spec.ts
+++ apps/skygrid/src/app/components/skygrid-logo-svg/skygrid-logo-svg.component.spec.ts
@@ -1,17 +1,14 @@
-import { ComponentFixture } from '@angular/core/testing';
-
 import { SkygridLogoSvgComponent } from './skygrid-logo-svg.component';
 import { render } from '@testing-library/angular';
 
 describe('SkyGridLogoSvgComponent', () => {
   let component: SkygridLogoSvgComponent;
-  let fixture: ComponentFixture<SkygridLogoSvgComponent>;
 
   beforeEach(async () => {
     const skiGridLogo = await render(SkygridLogoSvgComponent);
 
     component = skiGridLogo.fixture.componentInstance;
-    fixture.detectChanges();
+    skiGridLogo.detectChanges;
   });
 
   it('should create', () => {
